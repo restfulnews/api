26d02b613e118182088d059d2a4c935aaecf080c
Chris Joy <chrisjoydev@gmail.com>
2018-03-13 13:21:50 +1100

    Refactored company controller

--- src/api/company/controller.js
+++ src/api/company/controller.js
@@ -5,7 +5,7 @@ const config = require('../../config');
 const sendMail = require('../../services/sendgrid');
 const templates = require('../../services/templates');
 const { success, notFound } = require('../../services/response/');
-const Entity = require('./model');
+const Key = require('./model');
 
 /**
 * CUSTOM FUNCTIONS
@@ -17,36 +17,36 @@ const Entity = require('./model');
 */
 
 exports.create = ({ body, user }, res, next) => {
-	return Entity.create(body)
-		.then(entity => entity.view(true))
-		.then((entity) => res.status(201).json(entity))
+	return Key.create(body)
+		.then(key => key.view(true))
+		.then((key) => res.status(201).json(key))
 		.catch(next);
 };
 
 exports.index = ({ querymen: { query, select, cursor } }, res, next) =>
-	Entity.find({ archived: { $ne: true } }, select, cursor)
-		.then(entity => entity.map(entityObject => entityObject.view(true)))
+	Key.find({ archived: { $ne: true } }, select, cursor)
+		.then(key => key.map(entityObject => entityObject.view(true)))
 		.then(success(res))
 		.catch(next);
 
 exports.show = ({ params }, res, next) =>
-	Entity.findById(params.id)
+	Key.findById(params.id)
 		.then(notFound(res))
-		.then(entity => (entity ? entity.view() : null))
+		.then(key => (key ? key.view() : null))
 		.then(success(res))
 		.catch(next);
 
 exports.update = ({ body, params }, res, next) =>
-	Entity.findById(params.id)
+	Key.findById(params.id)
 		.then(notFound(res))
-		.then(entity => (entity ? _.extend(entity, body).save() : null))
-		.then(entity => (entity ? entity.view(true) : null))
+		.then(key => (key ? _.extend(key, body).save() : null))
+		.then(key => (key ? key.view(true) : null))
 		.then(success(res))
 		.catch(next);
 
 exports.destroy = ({ params }, res, next) =>
-	Entity.findById(params.id)
+	Key.findById(params.id)
 		.then(notFound(res))
-		.then(entity => (entity ? entity.remove() : null))
+		.then(key => (key ? key.remove() : null))
 		.then(success(res, 204))
 		.catch(next);
