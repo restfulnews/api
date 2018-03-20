8be67078b47a7c3897f0e90eb5cc60954892d3ca
Chris Joy <chrisjoydev@gmail.com>
2018-03-20 16:01:26 +1100

    Add docs to repo

---
+++ docs/api_project.js
@@ -0,0 +1,13 @@
+define({
+  "name": "restful-news",
+  "version": "0.0.1",
+  "description": "",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-03-20T04:57:50.741Z",
+    "url": "http://apidocjs.com",
+    "version": "0.16.1"
+  }
+});
