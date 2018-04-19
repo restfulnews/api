a84fd4fac67ed53fd5d0a47d3107d0aa38161b27
Chris Joy <chrisjoydev@gmail.com>
2018-04-20 00:02:26 +1000

    Basic api error handling

--- src/api/search/controller.js
+++ src/api/search/controller.js
@@ -7,9 +7,26 @@ const { responseWrapper } = require('../../services/response');
 */
 
 exports.search = asyncHandler(async ({ query, user }, res) => {
+	let results = [];
+	const warnings = [];
+	const startTime = new Date();
 	const cleanQuery = removeEmptyParams(query);
-	if (!cleanQuery.start_date) cleanQuery.start_date = new Date(new Date().setFullYear(new Date().getFullYear() - 5)).toISOString();
-	if (!cleanQuery.end_date) cleanQuery.end_date = new Date().toISOString();
-	const results = await Searcher(cleanQuery, user);
-	res.json(responseWrapper(results));
+	if (!cleanQuery.start_date) {
+		const fiveYearsFromNow = new Date().setFullYear(new Date().getFullYear() - 5);
+		cleanQuery.start_date = new Date(fiveYearsFromNow).toISOString();
+		warnings.push('Start time not specified.');
+	}
+	if (!cleanQuery.end_date) {
+		cleanQuery.end_date = new Date().toISOString();
+		warnings.push('End time not specified.');
+	}
+	if (cleanQuery.end_date < cleanQuery.start_date) warnings.push('End date cannot be before start date.');
+	if (!cleanQuery.topics) warnings.push('Topics not specified.');
+	if (!cleanQuery.companyids) warnings.push('Company id\'s not specified.');
+	try {
+		results = await Searcher(cleanQuery, user);
+	} catch (err) {
+		warnings.push(err);
+	}
+	res.json(responseWrapper(results, warnings, startTime, cleanQuery));
 }, 'Unable to retrieve news articles.');
