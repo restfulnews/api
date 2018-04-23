c7d816e1fad2b6c2bd8f9f6a423b8b7f3b3f8d4f
Lachlan Jones <rid.3r@hotmail.com>
2018-04-23 14:28:16 +1000

    Completed but untested speedtesting script

--- effeciencyTesting.pl
+++ effeciencyTesting.pl
@@ -9,11 +9,52 @@ die "Usage: \n$0 [NewsKeywordFile.txt]\n" if @ARGV < 1;
 my $newsTopicsFile = $ARGV[0];
 open my $fh, "<", $newsTopicsFile or die "Could not open input file $newsTopicsFile\n";
 open my $output, ">", "testResults.txt" or die "Could not open results file testResults.txt\n";
+print $output "";
+close $output;
+
+my $token = `curl --request POST --url http://api.restfulnews.com/auth --header 'content-type: application/json' --data '{ "email": "steven\@restfulnews.com", "password": "accident8" }'`;
+$token =~ s/^.+token\":\"(.*?)\".*$/$1/;
+print "token: [$token]\n";
+die "Invalid token\n" if $token =~ /[\[\{\]\}]\"/;
+
+open my $output, ">>", "testResults.txt" or die "Could not open results file testResults.txt\n";
+print $output "Timing data for News Searching\n";
+
 
 foreach my $line (<$fh>) {
     my @words = split(/[ ,\.\/]/, $line);
     foreach my $word (@words) {
         chomp $word;
-        print "$word\n";
+        print $output "\nTopic: $word\n";
+        my $url = "http://api.restfulnews.com/search?topics=";
+        $url .= $word;
+        $url .= "&start_date=2011-02-22T23:39:03.000Z&end_date=2018-02-22T23:39:03.000Z";
+        $url =~s/\r//;
+        my $timing = `time curl --request GET --url '$url' --header 'authorization: Bearer $token' --header 'content-type: application/json'`;
+        print $output "$timing\n";
     }
 }
+close $fh;
+close $output;
+
+open my $results, "<", "testResults.txt" or die "Failed to open results file\n";
+my $totalSecs = 0;
+my $numTopics = 0;
+foreach my $line (<$results>) {
+    if ($line =~ /real/) {
+        $line =~ s/^.*real\s(\d+m\d+\.\d+s).*$/$1/;
+        my $min = $line;
+        $min =~ s/(\d+)m.+$/$1/;
+        $min = $min * 60;
+        my $sec = $line;
+        $sec =~ s/^.+m(\d+.\d+).*$/$1/;
+        $sec = $sec + 0;
+        $totalSecs += $min + $sec;
+        $numTopics += 1;
+    }
+}
+
+printf ("Average Search time:\n%.4f seconds\n", $totalSecs/$numTopics);
+
+`rm testResults.txt`;
+print "All cleaned up!\n";
